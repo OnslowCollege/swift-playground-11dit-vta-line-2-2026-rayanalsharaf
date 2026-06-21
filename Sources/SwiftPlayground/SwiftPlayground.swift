e39db832b1548dfd2e5dd23ed3d2d7c4d37d7c0a
Rayan Alsharaf <rayan.alsharaf@student.onslow.school.nz>
2026-06-21 23:13:23 +0000

    Not finished

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -627,66 +627,107 @@ Task - Only to accept people in if they are over the age of 18 */
         // }
 
 
-        // Practice assesment - Sleep tracker 
-        print("Welcome to Sleep Tracker.")
+        // // Practice assesment - Sleep tracker 
+        // print("Welcome to Sleep Tracker.")
         
-        // Creating constants for the list of days in the week from Monday to Friday. 
-        let goodSleepTime: Int = 8
-        let okSleepTime: Int = 6
-        let days: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-        // Sets it at 0 so when we want to call a String from the list we can add 1 to it every time it asks the question.
-        var day = 0
-        var totalSleepTime: Int = 0
+        // // Creating constants for the list of days in the week from Monday to Friday. 
+        // let goodSleepTime: Int = 8
+        // let okSleepTime: Int = 6
+        // let days: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+        // // Sets it at 0 so when we want to call a String from the list we can add 1 to it every time it asks the question.
+        // var day = 0
+        // var totalSleepTime: Int = 0
 
-        // Starts the while loop and the Bool allows isRunning to always be either true or false
-        var isRunning: Bool = true
+        // // Starts the while loop and the Bool allows isRunning to always be either true or false
+        // var isRunning: Bool = true
     
-        // while loop asking the question.
-        while isRunning {
+        // // while loop asking the question.
+        // while isRunning {
 
-            // Reads the list of days and once we get to the end of the list it stops the while loop.
-            if day >= days.count {
-                let avgSleepTime = totalSleepTime / days.count
-                print("You slept for a total \(totalSleepTime) hours, average \(avgSleepTime) hours a day.")
+        //     // Reads the list of days and once we get to the end of the list it stops the while loop.
+        //     if day >= days.count {
+        //         let avgSleepTime = totalSleepTime / days.count
+        //         print("You slept for a total \(totalSleepTime) hours, average \(avgSleepTime) hours a day.")
 
-                if avgSleepTime >= goodSleepTime {
-                    print("You have a good sleep schedule.")
-                }
-                else if avgSleepTime >= okSleepTime {
-                    print("You have an ok sleep schedule")
-                } else {
-                    print("You have a bad sleep schedule")
-                }
+        //         if avgSleepTime >= goodSleepTime {
+        //             print("You have a good sleep schedule.")
+        //         }
+        //         else if avgSleepTime >= okSleepTime {
+        //             print("You have an ok sleep schedule")
+        //         } else {
+        //             print("You have a bad sleep schedule")
+        //         }
 
-                // Ends the while loop
-                isRunning = false
-                break
-            }
+        //         // Ends the while loop
+        //         isRunning = false
+        //         break
+        //     }
             
-            // Creates the constant to set the ideal sleep time bounderies from 3 - 12
-            let minSleepTime = 3
-            let maxSleepTime = 12
-            // Asks how many hours of sleep the user got and on which day
-            print("How many hours of sleep did you get on \(days[day])?")
-            if let userInput = Int(readLine()!) {
-            // If the user typed a valid integer between 3 - 12
-            if userInput > minSleepTime && userInput < maxSleepTime {
-            totalSleepTime += userInput
-            // prints how many hours of sleep the user got
-            print("Added \(userInput) hours.")
-            // Does plus 1 every time it asks the question so it goes down the list
-            day += 1
-            }
-            // if you enter a integer outside the boundaries
-            else {
-                print("Please type a valid integer from 3 to 12.")
-            }
+        //     // Creates the constant to set the ideal sleep time bounderies from 3 - 12
+        //     let minSleepTime = 3
+        //     let maxSleepTime = 12
+        //     // Asks how many hours of sleep the user got and on which day
+        //     print("How many hours of sleep did you get on \(days[day])?")
+        //     if let userInput = Int(readLine()!) {
+        //     // If the user typed a valid integer between 3 - 12
+        //     if userInput > minSleepTime && userInput < maxSleepTime {
+        //     totalSleepTime += userInput
+        //     // prints how many hours of sleep the user got
+        //     print("Added \(userInput) hours.")
+        //     // Does plus 1 every time it asks the question so it goes down the list
+        //     day += 1
+        //     }
+        //     // if you enter a integer outside the boundaries
+        //     else {
+        //         print("Please type a valid integer from 3 to 12.")
+        //     }
 
-            // if you do not enter a integer
-            } else {
-                print("Please type a valid integer from 3 to 12.")
-            }
+        //     // if you do not enter a integer
+        //     } else {
+        //         print("Please type a valid integer from 3 to 12.")
+        //     }
+        // }
+
+        // Practice Assesment - Screen Time Tracker (Graded)
+        print("Welcome to Screen Time Tracker")
+        let days: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+        let apps : [String] = ["Facebook", "Snapchat", "Instagram", "Discord", "Other"]
+        let screenTimeLimit: Int = Int(readLine()!)!
+        let hourLimit: Int = 0
+        let maxScreenTime: Int = 24
+        let minScreenTime: Int = 0
+
+        var day = 0
+        var app = 0
+        var isRunning: Bool = true
+        var isLimiting: Bool = true
+        
+
+        while isLimiting {
+            print("What is the hour limit set for each day?")
+            print("Enter a number:")
+            let limitInput: Int = Int(readLine()!)!
         }
+
+        while isRunning {
+            print("On \(day) how many hours did you spend on:")
+            let userInput: Int = Int(readLine()!)!
+            print("\(app):")
+
+            if userInput >= minScreenTime && userInput < maxScreenTime (
+                app += 1
+        }
+        
+         else {
+            print("Enter a valid integer from 0 to 24")
+        }
+
+    } else {
+        print("Enter a valid integer from 0 to 24")
+    }
+    
+
+
         
 
 
