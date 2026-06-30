c3410f80f765000472a65d520bf89afa990ea26f
Rayan Alsharaf <rayan.alsharaf@student.onslow.school.nz>
2026-06-30 21:58:15 +0000

    need to fix expected expression

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -8,53 +8,50 @@ struct SwiftPlayground {
         print("Welcome to Screen Time Tracker")
         let days: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
         let apps : [String] = ["Facebook", "Snapchat", "Instagram", "Discord", "Other"]
-        let hourLimit = 0
         let maxScreenTime = 24
         let minScreenTime = 0 
         let minScreenTimeLimit = 0 
         let maxScreenTimeLimit = 5 
         let minHoursApps: Int = 0
         let maxHoursApps: Int = 24
-        var day = 0
-        var app = 0
-        var isRunning: Bool = true
+        let summaryHours: Int = 0
+        let screenTimeLimit: Int = 0
         var totalHours: Int = 0
-        var isAppsRunning: Bool = true
+        var maximumHours: Int = 0
         var appScreenTime: Int = 0
         var isAgainRunning: Bool = true
-        var summaryHours: Int = 0
-        var dailyScreenTime: Int = 0
-        var screenTimeLimit: Int = 0
-        var max
+        var isAppsRunning: Bool = true
+        var isRunning: Bool = true
         
 
         while isRunning {
             print("What is the hour limit set for each day?")
             print("Enter a number:")
-            let limitInput: Int = Int(readLine()!)!
+            var limitInput: Int = Int(readLine()!)!
             if limitInput <= maxScreenTimeLimit && limitInput >= minScreenTimeLimit {
-                limitInput = screenTimeLimit
+                limitInput += screenTimeLimit
+                maximumHours += (screenTimeLimit * maxScreenTimeLimit) 
                 
                 print("This is your screen time limit: \(screenTimeLimit) hours")
                 isRunning = false
                 
             } else {
-                print("Enter a valid integer from\(minScreenTimeLimit) to \(maxScreenTimeLimit)")
+                print("Enter a valid integer from \(minScreenTimeLimit) to \(maxScreenTimeLimit)")
             }
-        
+         
             else {
-                print("Enter a valid integer from\(minScreenTimeLimit) to \(maxScreenTimeLimit) ")
+                print("Enter a valid integer from \(minScreenTimeLimit) to \(maxScreenTimeLimit)")
             }
             
-
         }
+        
 
         while isAgainRunning {
             for day in days {
             print("On \(day) how many hours did you spend on:")
             let dailyScreenTime: Int = Int(readLine()!)!
             totalHours += dailyScreenTime
-            }
+            
                 for app in apps {
             while isAppsRunning {
             let appInput: Int = Int(readLine()!)!
@@ -67,13 +64,13 @@ struct SwiftPlayground {
                 isAgainRunning = false
 
                 } else {
-                print("Enter a valid integer from\(minHoursApps) to \(maxHoursApps)")
-            }
-        
+                print("Enter a valid integer from \(minHoursApps) to \(maxHoursApps)")
+                }
+                
             else {
-                print("Enter a valid integer from\(minHoursApps) to \(maxHoursApps) ")
+                print("Enter a valid integer from \(minHoursApps) to \(maxHoursApps)")
             }
-            
+                
             }
             }
             if totalHours <= maxScreenTime && totalHours >= minScreenTime {
@@ -85,13 +82,26 @@ struct SwiftPlayground {
             }
         
             else {
-                print("Enter a valid integer from\(minHoursApps) to \(maxHoursApps) ")
+                print("Enter a valid integer from \(minHoursApps) to \(maxHoursApps)")
             }
             }
+            }
+        
 
             print("Summary:")
-            if summaryHours 
             print("This week you spent \(summaryHours) hours doom scrolling.")
+            let averageScreenTime = summaryHours / maxScreenTimeLimit
+            print("Average of \(averageScreenTime) hours per day")
+            if averageScreenTime <= maxScreenTimeLimit && totalHours >= minScreenTimeLimit {
+                print("Average of \(averageScreenTime) hours per day")
+                print("Overall your usage is average")
+            
+            } else {
+                print("Overall your usage is over the limit")
+            }
+        
+            
+            
 
 
 
